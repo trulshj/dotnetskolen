24eb97fe01cc984de66a80d63268aa9d53b8c348
Christian Peter <christian@peter.no>
2021-09-29 11:56:04 +0200

    Steg 6

---
+++ src/api/Domain.fs
@@ -0,0 +1,27 @@
+namespace NRK.Dotnetskolen
+
+module Domain = 
+
+    open System
+    open System.Text.RegularExpressions
+    
+    type Sending = {
+        Tittel: string
+        Kanal: string
+        StartTidspunkt: DateTimeOffset
+        SluttTidspunkt: DateTimeOffset
+    }
+
+    type Epg = Sending list
+    
+    
+    let isTitleValid (title: string) : bool =
+        let titleRegex = Regex(@"^[\p{L}0-9\.,-:!]{5,100}$")
+        titleRegex.IsMatch(title)
+        
+    let isChannelValid (channel: string) : bool =
+        channel.Equals("NRK1") || channel.Equals("NRK2")
+        
+        
+    let areStartAndEndTimesValid (startTime: DateTimeOffset) (endTime: DateTimeOffset) =
+        startTime.CompareTo(endTime) < 0
---
+++ test/unit/Tests.fs
@@ -0,0 +1,60 @@
+module Tests
+
+open System
+open Xunit
+open NRK.Dotnetskolen.Domain
+
+[<Theory>]
+[<InlineData("abc12")>]
+[<InlineData(".,-:!")>]
+[<InlineData("ABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJ")>]
+let ``isTitleValid valid title returns true`` (title: string) =
+    let isTitleValid = isTitleValid title
+
+    Assert.True isTitleValid
+
+[<Theory>]
+[<InlineData("abcd")>]
+[<InlineData("@$%&/")>]
+[<InlineData("abcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghija")>]
+let ``isTitleValid invalid title returns false`` (title: string) =
+    let isTitleValid = isTitleValid title
+
+    Assert.False isTitleValid
+    
+[<Theory>]
+[<InlineData("NRK1")>]
+[<InlineData("NRK2")>]
+let ``isChannelValid valid channel returns true`` (channel: string) =
+    let isChannelValid = isChannelValid channel
+
+    Assert.True isChannelValid
+
+[<Theory>]
+[<InlineData("nrk1")>]
+[<InlineData("NRK3")>]
+let ``isChannelValid invalid channel returns false`` (channel: string) =
+    let isChannelValid = isChannelValid channel
+
+    Assert.False isChannelValid
+    
+    
+    
+[<Fact>]
+let ``areStartAndEndTimesValid start before end returns true`` () =
+    let startTime = DateTimeOffset.Now
+    let endTime = startTime.AddMinutes 30.
+
+    let areStartAndSluttTidspunktValid = areStartAndEndTimesValid startTime endTime
+
+    Assert.True areStartAndSluttTidspunktValid
+    
+    
+[<Fact>]
+let ``areStartAndEndTimesValid end before start returns false`` () =
+    let startTime = DateTimeOffset.Now
+    let endTime = startTime.Subtract(TimeSpan.FromMinutes(30.0))
+
+    let areStartAndSluttTidspunktValid = areStartAndEndTimesValid startTime endTime
+
+    Assert.False areStartAndSluttTidspunktValid
