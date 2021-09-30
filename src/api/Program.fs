2dd7cee6696db76219c50012e552d5bea3f78c24
Ingvild Forseth <ingvild.forseth@nrk.no>
2021-09-30 14:54:50 +0200

    step-9-dependency-injection

--- src/api/Program.fs
+++ src/api/Program.fs
@@ -5,12 +5,16 @@ module Program =
     open Microsoft.Extensions.Hosting
     open Microsoft.AspNetCore.Hosting
     open Microsoft.AspNetCore.Builder
+    open Microsoft.Extensions.DependencyInjection
+
+    let configureServices (webHostContext: WebHostBuilderContext) (services: IServiceCollection) =
+      ()
 
     let configureApp (webHostContext: WebHostBuilderContext) (app: IApplicationBuilder) =
       ()  
 
     let createHostBuilder args =
-        Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(fun webHostBuilder -> webHostBuilder.Configure(configureApp) |> ignore)
+        Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(fun webHostBuilder -> webHostBuilder.Configure(configureApp).ConfigureServices(configureServices) |> ignore)
 
     [<EntryPoint>]
     let main argv =
