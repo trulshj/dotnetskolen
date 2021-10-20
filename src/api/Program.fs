8a2716fb63d74fbb3c6a0aca02e8045f2e6b252e
Egil Lillehaug <Egil.Lillehaug@nrk.no>
2021-10-20 09:28:49 +0200

    9 del 1

--- src/api/Program.fs
+++ src/api/Program.fs
@@ -1,21 +1,43 @@
 // Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
 
-open System
-open NRK.Dotnetskolen.Domain
+// open System
+// open NRK.Dotnetskolen.Domain
+//
+// // Define a function to construct a message to print
+// let from whom =
+//     sprintf "from %s" whom
+//
+// [<EntryPoint>]
+// let main argv =
+//     let epg = [
+//         {
+//             Tittel = "Dagsrevyen"
+//             Kanal = "NRK1"
+//             StartTidspunkt = DateTimeOffset.Parse("2021-04-16T19:00:00+02:00")
+//             SluttTidspunkt = DateTimeOffset.Parse("2021-04-16T19:30:00+02:00")
+//         }
+//     ]
+//     printfn "%A" epg
+//     0 // return an integer exit code
+namespace NRK.Dotnetskolen.Api
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+module Program =
 
-[<EntryPoint>]
-let main argv =
-    let epg = [
-        {
-            Tittel = "Dagsrevyen"
-            Kanal = "NRK1"
-            StartTidspunkt = DateTimeOffset.Parse("2021-04-16T19:00:00+02:00")
-            SluttTidspunkt = DateTimeOffset.Parse("2021-04-16T19:30:00+02:00")
-        }
-    ]
-    printfn "%A" epg
-    0 // return an integer exit code
+    open Microsoft.Extensions.Hosting
+
+    open Microsoft.AspNetCore.Hosting
+    open Microsoft.AspNetCore.Builder
+
+    let configureApp (webHostContext: WebHostBuilderContext) (app: IApplicationBuilder) =
+        ()
+
+    let createHostBuilder args =
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(fun webHostBuilder ->
+                webHostBuilder.Configure(configureApp) |> ignore
+            )
+
+    [<EntryPoint>]
+    let main argv =
+        createHostBuilder(argv).Build().Run()
+        0
