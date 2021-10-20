f7435b255e64ea65eefd9937f268095d6ab9885a
Egil Lillehaug <Egil.Lillehaug@nrk.no>
2021-10-20 09:52:19 +0200

    pingtest
    klar for "Test for ping", steg 9

--- src/api/Program.fs
+++ src/api/Program.fs
@@ -28,13 +28,23 @@ module Program =
     open Microsoft.AspNetCore.Hosting
     open Microsoft.AspNetCore.Builder
 
+    open Microsoft.Extensions.DependencyInjection
+    open Giraffe
+
     let configureApp (webHostContext: WebHostBuilderContext) (app: IApplicationBuilder) =
-        ()
+      let webApp = route "/ping" >=> text "pong"
+      app.UseGiraffe webApp
+
+
+    let configureServices (webHostContext: WebHostBuilderContext) (services: IServiceCollection) =
+      services.AddGiraffe() |> ignore
 
     let createHostBuilder args =
         Host.CreateDefaultBuilder(args)
             .ConfigureWebHostDefaults(fun webHostBuilder ->
-                webHostBuilder.Configure(configureApp) |> ignore
+                webHostBuilder
+                    .Configure(configureApp)
+                    .ConfigureServices(configureServices) |> ignore
             )
 
     [<EntryPoint>]
