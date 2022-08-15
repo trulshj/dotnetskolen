045097c68901c94280d9c17f606a58b903dc67d8
Truls Henrik <thj@variant.no>
2022-08-15 16:23:33 +0200

    Add basic Asp and integration test for it

--- src/api/Program.fs
+++ src/api/Program.fs
@@ -1,12 +1,17 @@
-﻿open System
-open NRK.Dotnetskolen.Domain
+﻿namespace NRK.Dotnetskolen.Api
 
-let epg = [
-    {
-        Tittel = "Dagsrevyen"
-        Kanal = "NRK1"
-        Starttidspunkt = DateTimeOffset.Parse("2021-04-16T19:00:00+02:00")
-        Sluttidspunkt = DateTimeOffset.Parse("2021-04-16T19:30:00+02:00")
-    }
-]
-printfn "%A" epg
+module Program =
+    open System
+    open Microsoft.AspNetCore.Builder
+
+    let createWebApplicationBuilder () =
+        WebApplication.CreateBuilder()
+
+    let createWebApplication (builder: WebApplicationBuilder) =
+        let app = builder.Build()
+        app.MapGet("/ping", Func<string>(fun () -> "pong")) |> ignore
+        app
+
+    let builder = createWebApplicationBuilder()
+    let app = createWebApplication(builder)
+    app.Run()
